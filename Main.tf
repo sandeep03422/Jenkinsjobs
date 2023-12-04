3012954a700c9ed33b6ae193d9a7a12530726744
sandeep03422 <143420018+sandeep03422@users.noreply.github.com>
2023-12-04 15:53:05 -0500

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-01b3482ea9e568d65"  # Change to your desired AMI
+ ami           = "ami-05fa00d4c63e32376" # us-west-2
   instance_type = "t2.micro"
   key_name      = "Devops"  # Change to your key pair name
 
